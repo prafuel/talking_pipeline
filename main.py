be75ea6e4c133ba07fe3a3fd084b0ddba92ef736
prafuel <official.sonawaneprafull@gmail.com>
2025-06-22 18:42:59 +0530

    faster-whisper code added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
-from src.STT import STT_whisper
+from src.STT import STT_fasterwhisper
 from src.TTS import TTS_whisperspeech
 
 audio_file = "10_Second_Talks.mp3"
 
 # Speech to Text
-text = STT_whisper(audio_file_path=audio_file)
+# text = STT_whisper(audio_file)
+text = STT_fasterwhisper(audio_file)
 
 print(text)
 
